7e5103a72f3ef1d4dd248c441cb0dd3c421b5775
Josse3 <josse.cottenier@hotmail.com>
2019-06-01 17:49:48 +0200

    Added home button

--- ui-react/src/components/Vocabularium/Vocabularium.js
+++ ui-react/src/components/Vocabularium/Vocabularium.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './Vocabularium.css';
 import Banner from '../Banner/Banner';
+import HomeButton from '../HomeButton/HomeButton';
 import Navboxline from './Navboxline/Navboxline';
 
 function Vocabularium() {
     return (
         <div className="vocabularium">
+            <HomeButton />
             <Banner text="Studeer vocablarium" />
             <Navboxline />
         </div>
