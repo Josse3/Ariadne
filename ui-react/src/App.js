468b36e6cf17cbfb2a77831d85315690ea812ccf
Josse3 <josse.cottenier@hotmail.com>
2019-06-01 17:06:42 +0200

    Added routing

--- ui-react/src/App.js
+++ ui-react/src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 
 import Main from './components/Main/Main';
 import CorrectionMode from './components/CorrectionMode/CorrectionMode';
+import Vocabularium from './components/Vocabularium/Vocabularium';
 import PocketMode from './components/PocketMode/PocketMode';
+import More from './components/More/More';
 import AddTool from './components/AddTool/AddTool';
 
 function App() {
@@ -16,7 +18,9 @@ function App() {
           <Switch>
             <Route path="/" exact component={Main} />
             <Route path="/correctionmode" exact component={CorrectionMode} />
-            <Route path="/pocketmode" exact component={PocketMode} />
+            <Route path="/vocabularium" exact component={Vocabularium} />
+            <Route path="/vocabularium/pocketmode" exact component={PocketMode} />
+            <Route path="/more" exact component={More} />
             <Route path="/addtool" exact component={AddTool} />
           </Switch>
         </Router>
