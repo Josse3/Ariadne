8d4027c6f914498539bbfacaa4d9b6af27dd361f
Josse3 <josse.cottenier@hotmail.com>
2019-06-01 20:37:19 +0200

    cleanup

--- ui-react/src/components/More/Selector/Selector.js
+++ ui-react/src/components/More/Selector/Selector.js
@@ -4,9 +4,7 @@ import "./Selector.css";
 class Selector extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
     this.state = {};
-
     this.handleChange = this.handleChange.bind(this);
     this.sendData = this.sendData.bind(this);
   }
@@ -31,48 +29,48 @@ class Selector extends React.Component {
     }
   }
 
+  componentDidMount() {
+    console.log(this.props);
+  }
+
   sendData() {
-      const data = this.state;
-      console.log(data.author);
-      console.log(this.props.sendInformation);
-      this.props.sendInformation(data.author);
+    const data = this.state;
+    this.props.sendInformation(data.author);
   }
 
   render() {
-    console.log(this.props);
-    console.log(this.props.working);
     const works = {
       herodotus: ["historiae"],
       xenophon: ["anabasis", "memorabilia"]
     };
 
     return (
-        <div className="selector">
-            <form className="select-work" onChange={this.handleChange}>
-            <select name="author">
-                {Object.keys(works).map(author => {
-                    return (
-                    <option value={author} key={`option_${author}`}>
-                        {author}
-                    </option>
-                    );
-                })}
-            </select>
-            <select name="work">
-                {works[this.state.author ? this.state.author : "herodotus"].map(work => {
-                    return (
-                        <option value={work} key={`option_${work}`}>
-                        {work}
-                        </option>
-                    );
-                    }
-                )}
-            </select>
+      <div className="selector">
+        <form className="select-work" onChange={this.handleChange}>
+          <select name="author">
+            {Object.keys(works).map(author => {
+              return (
+                <option value={author} key={`option_${author}`}>
+                  {author}
+                </option>
+              );
+            })}
+          </select>
+          <select name="work">
+            {works[this.state.author ? this.state.author : "herodotus"].map(work => {
+              return (
+                <option value={work} key={`option_${work}`}>
+                  {work}
+                </option>
+              );
+            }
+            )}
+          </select>
         </form>
         <button name="confirm" className="btn-submit" onClick={this.sendData}>
-            Bevestigen
+          Bevestigen
         </button>
-    </div>
+      </div>
     );
   }
 }
