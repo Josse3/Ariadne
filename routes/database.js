240a5dce740cfa818d6b77deec394130539479b6
Josse3 <josse.cottenier@hotmail.com>
2019-10-12 15:35:02 +0200

    Added id to every word in database to order consistently

--- routes/database.js
+++ routes/database.js
@@ -20,7 +20,7 @@ const authorize = (req, res, next) => {
 // Get requests
 // Get entire vocabularium
 router.get('/full', (ereq, eres, next) => {
-    pool.query('SELECT * FROM dictionary', (err, pres) => {
+    pool.query('SELECT id, word, genus, translation, page FROM dictionary ORDER BY id ASC', (err, pres) => {
         if (err) { console.log(err) };
         eres.send(pres);
     })
---
+++ ui-react/src/components/VocabulariumList/VocabulariumList.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import './VocabulariumList.css';
+import Header from '../Header/Header';
+import Ariadne from '../../util/Ariadne';
+
+function VocabulariumList() {
+    const [dictionary, setDictionary] = useState([]);
+    const [wordListHTML, setWordListHTML] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            fetch('/db/full')
+                .then(response => {
+                    if (!response.ok) throw Error('Failed fetching data');
+                    return response.json();
+                })
+                .then(jsonResponse => setDictionary(jsonResponse.rows));
+        }
+        fetchData();
+    }, []);
+
+    const provideListHTML = () => {
+        if (dictionary) {
+            setWordListHTML(
+                <div className="word-list">
+                    <div className="subst1-header">
+                        <p>#</p>
+                        <p>Woord</p>
+                        <p>Genus</p>
+                        <p>Vertaling</p>
+                        <p>Pagina</p>
+                    </div>
+                    {dictionary.map(wordObj => {
+                        return (
+                            <div key={`word-item-${wordObj.word}`} className="word-item">
+                                {Object.keys(wordObj).map(wordProperty => {
+                                    let displayedProperty;
+                                    if (wordProperty === 'word') {
+                                        displayedProperty = Ariadne.toGreek(wordObj[wordProperty]);
+                                    } else if (wordProperty === 'genus') {
+                                        displayedProperty = Ariadne.renderGenus(wordObj[wordProperty]);
+                                    } else {
+                                        displayedProperty = wordObj[wordProperty];
+                                    }
+                                    return <p key={`word-item-detail-${wordObj.word}-${wordProperty}`} className="word-item-detail">{displayedProperty}</p>
+                                })}
+                            </div>
+                        );
+                    })}
+                </div>)
+        }
+    }
+
+    useEffect(provideListHTML, [dictionary]);
+
+    return (
+        <div className="vocabularium-list">
+            <Header />
+            <h1 className="title">(Ἡδε ἡ χάρτη ἔτι κατασκευάζεται)</h1>
+            {wordListHTML}
+        </div>
+    )
+}
+
+export default VocabulariumList;
