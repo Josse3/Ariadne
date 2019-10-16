bd120a31e79a670dadf5a283320aa6c8b29679c9
Josse3 <josse.cottenier@hotmail.com>
2019-10-16 19:07:52 +0200

    Vocabularium list table + adapted to reusable styles

--- ui-react/src/components/AddTool/AddTool.jsx
+++ ui-react/src/components/AddTool/AddTool.jsx
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from 'react';
+// CSS
 import './AddTool.css';
+import '../../styles/vocabulariumlist.css';
+// Components
 import Header from '../Header/Header';
+// Util
 import Ariadne from '../../util/Ariadne';
 
 function AddTool() {
+    // Authentication page
     const [enteredPass, setEnteredPass] = useState('');
     const [authenticationFailed, setAuthenticationFailed] = useState(false);
-    const [phase, setPhase] = useState('authenticating');
+    // Edit interface
+    const [dictionary, setDictionary] = useState([]);
+    // Phase
+    const [phase, setPhase] = useState('editing');
 
+    // Sends request to backend which replies with boolean defining whether or not the authentication was succesful
     const authenticate = () => {
         fetch(`/authentication/addtool?password=${enteredPass}`)
             .then(response => {
@@ -16,6 +25,18 @@ function AddTool() {
             })
             .then(jsonResponse => jsonResponse === true ? setPhase('editing') : setAuthenticationFailed(true));
     };
+
+    // Fetches the dictionary data as soon as the user gets access to the edit interface
+    useEffect(() => {
+        if (phase === 'editing') {
+            fetch('/db/full')
+                .then(response => {
+                    if (!response.ok) throw Error('Failed fetching dictionary data from backend.');
+                    return response.json();
+                })
+                .then(setDictionary);
+        }
+    }, [phase])
 
     return (
         <div className="add-tool">
@@ -38,6 +59,25 @@ function AddTool() {
 
             {phase === 'editing' && (
                 <div className="edit-interface">
+                    <h1>Substantieven eerste vervoeging</h1>
+                    <div className="vocabularium-list-grid vocabularium-list-subst1">
+                        <div className="subst1-header">
+                            <p>#</p>
+                            <p>Woord</p>
+                            <p>Geslacht</p>
+                            <p>Vertaling</p>
+                            <p>Pagina</p>
+                        </div>
+                        {dictionary.map(wordObj => {
+                            return (
+                                <div className="word-item-subst1" key={`word-item-${wordObj.word}`}>
+                                    {Object.entries(wordObj).map(entryObj => {
+                                        return <p key={entryObj[0]}>{entryObj[1]}</p>
+                                    })}
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
             )}
         </div>
