4c6ed37c3aab6969a79194af3e348cd31ae607e7
Josse3 <josse.cottenier@hotmail.com>
2019-10-16 17:27:50 +0200

    Quit using variables of HTML + small styling fix

--- ui-react/src/components/AddTool/AddTool.jsx
+++ ui-react/src/components/AddTool/AddTool.jsx
@@ -7,42 +7,29 @@ function AddTool() {
     const [enteredPass, setEnteredPass] = useState('');
     const [phase, setPhase] = useState('authenticating');
 
-    const authenticate = () => setPhase('entering');
-
-    let displayHTML;
-
-    const authenticationHTML = (
-        <form className="auth-form">
-            <input
-                type="password"
-                className="auth-input"
-                placeholder="Voer authenticatie-code in"
-                onChange={e => setEnteredPass(e.target.value)}
-            />
-            <button className="auth-btn" onClick={authenticate}>
-                Meld aan
-            </button>
-        </form>
-    )
-
-    const enteringHTML = (
-        <div className="input-form">
-            
-        </div>
-    )
-
-    if (phase === 'authenticating') {
-        displayHTML = authenticationHTML;
-    } else if (phase === 'entering') {
-        displayHTML = enteringHTML;
-    } else {
-        throw Error('Invalid phase');
-    }
+    const authenticate = () => setPhase('editing');
 
     return (
         <div className="add-tool">
             <Header />
-            {phase === 'authenticating' && authenticationHTML}
+            {phase === 'authenticating' && (
+                <form className="auth-form">
+                    <input
+                        type="password"
+                        className="auth-input"
+                        placeholder="Voer authenticatie-code in"
+                        onChange={e => setEnteredPass(e.target.value)}
+                    />
+                    <button className="auth-btn" onClick={authenticate}>
+                        Meld aan
+                    </button>
+                </form>
+            )}
+
+            {phase === 'editing' && (
+                <div className="edit-interface">
+                </div>
+            )}
         </div>
     )
 }
